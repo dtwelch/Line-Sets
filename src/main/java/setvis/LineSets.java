e603443d96c1e8f6a45b9c80090e56ba2665e69a
Daniel <daniel@Daniels-MacBook-Pro.local>
2014-12-05 18:30:31 -0500

    initial commit

---
+++ src/main/java/setvis/LineSets.java
@@ -0,0 +1,16 @@
+package setvis;
+
+import processing.core.PApplet;
+
+public class LineSets extends PApplet {
+
+    @Override
+    public void setup() {
+        size(500, 500);
+        System.out.println("Hello world");
+    }
+
+    public static void main(String[] args) {
+        PApplet.main("setvis.LineSets");
+    }
+}
